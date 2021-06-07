c5addb2e3837031ead2f4b0ca3fed4f38868ba56
ijinfeng <jinfeng@yunjigobal.com>
2021-06-07 21:59:00 +0800

    log color

---
+++ log_color.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/ruby
+
+class Color
+    def self.natural 
+        0
+    end
+    def self.black 
+        30 
+    end
+    def self.red 
+        31 
+    end
+    def self.green 
+        32 
+    end
+    def self.yellow 
+        33 
+    end
+    def self.blue 
+        34 
+    end
+    def self.magenta 
+        35 
+    end
+    def self.cyan 
+        36 
+    end
+    def self.white 
+        37 
+    end
+end
+
+def color_text(text, color = Color.natural)
+    if color == 0
+        return text
+    end
+    return "\033[#{color}m#{text}\033[0m"
+end
+
+puts color_text('Hello') + color_text(' jinfeng ', Color.red) + color_text('nihao!', Color.white)
